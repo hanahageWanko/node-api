bd0d16777138baac4e9390368181deb21ac28c09
nakagawa <237s10mimi@gmail.com>
2020-08-30 01:19:08 +0900

    add get edit/:id

--- viewer/src/App.vue
+++ viewer/src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <GlossaryList msg="Welcome to Your Vue.js App"/>
+  <div id="wrapper">
+    <GlossaryList />
+    <GlossaryDetail />
+    
   </div>
 </template>
 
 <script>
 import GlossaryList from './components/GlossaryList.vue'
-
+import GlossaryDetail from './components/GlossaryDetail.vue'
 export default {
   name: 'App',
   components: {
-    GlossaryList
+    GlossaryList,
+    GlossaryDetail
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fafafa;
+}
+#wrapper {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  background-color: #272822;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 0;
+  position: relative;
+  overflow: hidden;
+}
+
+#wrapper > div {
+  flex:1;
 }
 </style>
---
+++ viewer/src/components/GlossaryDetail.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="note">{{glossary}}</div>
+</template>
+
+<script>
+import Api from '@/plugins/Api'
+export default {
+  name: 'GlossaryDetail',
+    data() {
+    return{
+     glossary:[]
+    }
+  },
+  methods: {
+    // サーバーから返ってくる値をログに出力したいのでasyncとawaitを行う
+    post :async function() {
+      let response = await Api.getGlossary(16)
+      const { data } = response;
+      console.log(data.result[0])
+      this.glossary = data.result[0];
+    },
+  },
+  mounted(){
+    this.post();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.glossary-list {
+  margin: 10px;
+  padding: 15px;
+  border-radius: 7px;
+  background-color: #1D1E1B;
+  cursor:pointer;
+}
+.glossary-list-title {
+  font-size:130%;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  overflow-y:scroll;
+  height:100vh;
+  padding: 10px;
+}
+li {
+  display: inline-block;
+  margin: 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
