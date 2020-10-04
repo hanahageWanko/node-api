4419a7f61731cc05615916e58cc6098832862945
nakagawa <237s10mimi@gmail.com>
2020-10-05 02:24:08 +0900

    remove typescript

---
+++ Server.js
@@ -0,0 +1,38 @@
+import Sequelize from 'sequelize';
+import cls from 'continuation-local-storage';
+const namespace = cls.createNamespace('my-namespace');
+// Sequelize.useCLS(namespace);
+
+const sequelize = new Sequelize('Todo','root','', {
+  host : 'localhost',
+  dialect: 'mysql',
+
+    pool:{
+      max: 10,
+      min: 3,
+      acquire: 30000,
+      idle: 10000
+  },
+
+  operationsAliases: false
+});
+
+const models = {
+  Task: sequelize.define(
+    'task',
+    {
+      name : Sequelize.STRING,
+      discription : Sequelize.STRING,
+    },
+    {
+      freezeTableName : true,
+      timestamps: false
+    }
+  )
+}
+
+module.exports = {
+  sequelize: sequelize,
+  models: models
+}
+
