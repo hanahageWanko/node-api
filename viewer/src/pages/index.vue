5799e9ec3b47f252fb552a55651a70e7b1d36edc
nakagawa <237s10mimi@gmail.com>
2020-11-07 12:00:00 +0900

    delete some file

--- viewer/src/pages/index.vue
+++ viewer/src/pages/index.vue
@@ -1,108 +1,53 @@
 <template>
   <div id="wrapper">
-    <!-- <GlossaryList
-      @getCurrentId="getGlossaryDetail($event)"
-      :Glossarys="GlossaryState"
-    />
-      <GlossaryDetail
-        v-if="CurrentGlossary.length >= 1"
-        :glossaryDetail="CurrentGlossary[0]"
-      />
-      <GlossaryAddForm v-else/> -->
-    <signUp v-if="!$route.query.user" />
-    <div v-else>
-      <!-- サインアップ後のメイン画面 -->
-      <!-- アコーディオンメニューを後でやる時 -->
-      <!-- <div>
-        <div
-          id="id_setup_panel"
-          v-on:click="toggleCtrlPanel"
-          class="cls_toggle_expand_collapse"
-        >
-          {{ 'オプション（＠' + userName + 'さん）' }}
-        </div>
-        <transition name="trans_slide">
-          <div
-            id="id_setup_transslide"
-            v-if="isPanelShow"
-            class="menu_slide_accordion"
-          >
-            ここにオプションのパネルを追加。
-          </div>
-      </div> -->
-      <div class="glossary-form">
-        <div class="textbox">
-          <input v-model="inputTitie" type="text" />
-        </div>
-        <div class="textbox">
-          <textarea
-            v-model="inputText"
-            placeholder="ここに入力する。複数行可。"
-          ></textarea>
-        </div>
-        <div>
-          <div @click="clickInputButton">リストに追加する</div>
-        </div>
-      </div>
-      <v-row class="glossary-list">
-        <v-col
-          v-for="(item, index) in GlossaryList"
-          :key="index"
-          cols="4"
-          class="glossary-list-item"
-        >
-          <v-card dark>
-            <v-card-title v-text="item.title" class="headline p-b-0" />
-            <v-card-text>
-              <div>{{ item.text }}</div>
-            </v-card-text>
-            <v-row align="center" class="d-flex" justify="end">
-              <v-col>
-                <v-card-text v-text="item.dateStr" />
-              </v-col>
-              <v-col cols="auto" class="p-0">
-                <v-row align="center">
-                  <v-col>
-                    <v-btn fab icon small>
-                      <v-icon>
-                        mdi-pencil
-                      </v-icon>
-                    </v-btn>
-                    <v-btn class="mr-5" fab icon small>
-                      <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-      </v-row>
-    </div>
+    <SignUp v-if="!$route.query.user" />
+    <GlosssaryAddForm v-if="$route.query.user" :username="UserName" />
+    <v-row v-if="$route.query.user" class="glossary-list">
+      <v-progress-linear v-if="createGlossaryFlg" indeterminate />
+      <v-col
+        v-for="(item, index) in GlossaryList"
+        :key="index"
+        cols="4"
+        class="glossary-list-item"
+      >
+        <v-card dark>
+          <v-card-title v-text="item.title" class="headline pb-0" />
+          <v-card-text>
+            <div>{{ item.text }}</div>
+          </v-card-text>
+          <v-row align="center" class="d-flex" justify="end">
+            <v-col>
+              <v-card-text v-text="item.dateStr" />
+            </v-col>
+            <v-col cols="auto" class="p-0">
+              <v-btn fab icon small>
+                <v-icon>
+                  mdi-pencil
+                </v-icon>
+              </v-btn>
+              <v-btn class="mr-5" fab icon small>
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 <script>
-// javascriptファイルをココへ。
-import signUp from '@/components/signUp'
-
-// import axios from 'axios'
+import SignUp from '@/components/SignUp'
+import GlosssaryAddForm from '@/components/GlossaryAddForm'
 import { mapState } from 'vuex'
 export default {
-  name: 'App', // 「el : "#id_app1"」としていた部分。
   components: {
-    signUp
+    SignUp,
+    GlosssaryAddForm
   },
   data() {
     // オブジェクト{}で定義していた値を、「その値を返却する（無名）関数」に書き換える。
     return {
-      setuppanel_text: 'オプションはこちら',
-      keyName: 'user',
-      itemStorage: null,
-      inputText: '',
-      inputTitie: '',
-      todoList: [],
-      targetKey: '',
-      isPanelShow: false
+      createGlossaryFlg: false
     }
   },
   computed: {
@@ -114,29 +59,11 @@ export default {
   },
   created() {
     if (this.$route.query.user) {
-      console.log(this.$route.query.user)
       this.$store.dispatch('user/setUserName', this.$route.query.user)
       this.$ItemStorage.fetch(this.UserName)
     }
   },
   methods: {
-    toggleCtrlPanel() {
-      this.isPanelShow = !this.isPanelShow
-    },
-    clickInputButton() {
-      const newText = this.inputText
-      const newTitle = this.inputTitie
-      const postData = {
-        title: newTitle,
-        text: newText
-      }
-      if (postData.title !== '' && postData.text !== '') {
-        this.$ItemStorage.add(this.UserName, postData).then((createdItem) => {
-          this.todoList.push(createdItem)
-        })
-        this.input_message = ''
-      }
-    },
     clickItem(index) {
       // this.todoList[index].toggleTextStyle('text-decoration: line-through;')
       // ToDo: クリックでのトグル動作時の扱いを『暫定』としたいので、このような実装にする。
@@ -192,41 +119,14 @@ strong {
   line-height: 1.65rem;
   letter-spacing: inherit;
 }
+
 #wrapper {
-  display: flex;
-  width: 100%;
-  height: 100vh;
   padding: 40px;
-  background-color: #272822;
-  justify-content: space-evenly;
   margin: 0;
   position: relative;
-  overflow: hidden;
 }
 #wrapper > div {
   flex: 1;
-}
-.glossary-form {
-  padding: 20px;
-  .textbox {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    label {
-      padding-right: 15px;
-      width: 20%;
-    }
-    input {
-      display: block;
-      width: 100%;
-      border: none;
-      background-color: transparent;
-      border-bottom: 1px solid rgba(255, 255, 255, 0.8);
-    }
-    + .textbox {
-      margin-top: 20px;
-    }
-  }
 }
 
 .d-flex {
@@ -238,8 +138,11 @@ strong {
   padding: 0;
 }
 
-.p-b-0 {
+.pb-0 {
   padding-bottom: 0;
+}
+.pt-0 {
+  padding-top: 0;
 }
 
 .glossary-list-item {
